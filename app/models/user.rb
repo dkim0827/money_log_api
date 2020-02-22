e94444425a70dcfdbb41e70d152f6dd290fe3952
DanielKim <dltcm0827@gmail.com>
2020-02-22 13:45:29 -0800

    Created User table, added validation on User model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+    # email must be present, need to be unique
+    validates :email, presence: true, uniqueness: true,
+    # format for proper email
+    format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    # from gem 'bcrypt'. adds password, password confirmation && validation
+    has_secure_password
+
+
+    def full_name
+        "#{first_name} #{last_name}".strip.squeeze
+    end
+end
