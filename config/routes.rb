38252e1563b2344cbebe7bfb8d154eb24e93eb76
DanielKim <dltcm0827@gmail.com>
2020-02-22 14:37:51 -0800

    Added middleware in application.rb for CORS, Cookies && Added router for users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # route to check current_user
+      get 'users/current', to: 'users#current'
+      # route to change password
+      patch '/users/:id/password_edit', to: 'users#password_update'
+
+      resource :session, only: [:create, :destroy]
+      resources :users
+  end
 end
